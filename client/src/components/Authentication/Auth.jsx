462eed9a190e00e7656732227f4c02e00018126a
Kevin Chen <kchen3568@gmail.com>
2020-04-25 17:21:03 -0500

    clean up repo

--- client/src/components/Authentication/Auth.jsx
+++ client/src/components/Authentication/Auth.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+// import { useHistory } from "react-router-dom";
 import Dashboard from '../Dashboard/Dashboard';
 import Landing from '../Landing/Landing';
+import SignIn from './SignIn';
 
 const Auth = (props) => {
+    // let history = useHistory();
+
     return ( <React.Fragment>
         <Switch>
             <Route exact path='/'>
                 { props.signedIn ? <Dashboard /> : <Landing /> }
+            </Route>
+            <Route path='/signin'>
+                { props.signedIn ? <Dashboard /> : <SignIn /> }
             </Route>
         </Switch>
     </React.Fragment> );
